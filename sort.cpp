66e12434e45ad2756e698c14772237b54f0280b7
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2019-12-20 11:04:32 +0800

    getter & setter of Key in List

---
+++ sort.cpp
@@ -0,0 +1,22 @@
+// 12/20, am11:00, 2019, by Queenie Chen
+// sort 
+
+class Element
+{
+    private:
+        int key;
+        // other fields
+
+    public:
+        
+        // getter & setter
+        int getKey() const 
+        {
+            return key;
+        };
+
+        void setKey(int insertedKey)
+        {
+            key = insertedKey;
+        };
+};
